aaf25e04ac3462d75ea5fa1e0311dbd93802d9f5
alsotang <alsotang@gmail.com>
2015-08-07 13:45:32 +0800

    add str_to_number.js

---
+++ benchmark/str_to_number.js
@@ -0,0 +1,40 @@
+var str = '100';
+
+suite('str_to_int_number', function () {
+  bench('+str', function () {
+    var a = +str;
+  })
+
+  bench('Number(str)', function () {
+    var a = Number(str)
+  })
+
+  bench('parseInt(str)', function () {
+    var a = parseInt(str)
+  })
+
+  bench('parseInt(str, 10)', function () {
+    var a = parseInt(str, 10)
+  })
+
+  bench('~~str', function () {
+    var a = ~~str
+  })
+
+  bench('str | 0', function () {
+    var a = str | 0
+  })
+
+  bench('str >> 0', function () {
+    var a = str >> 0
+  })
+
+  bench('str >>> 0', function () {
+    var a = str >>> 0
+  })
+
+  bench('str * 1', function () {
+    var a = str * 1
+  })
+})
+
